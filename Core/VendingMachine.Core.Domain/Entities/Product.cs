75c9bd9cbf21e7c0dc9659a604d6cfd1381f1f0f
andrew.severiukhin <and00998843@gmail.com>
2025-09-30 13:26:53 +0300

    add product class

---
+++ Core/VendingMachine.Core.Domain/Entities/Product.cs
@@ -0,0 +1,41 @@
+namespace VendingMachine.Core.Domain.Entities;
+
+public class Product
+{
+    private string _name;
+    public string Name
+    {
+        get => _name;
+
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Имя товара не может быть пустым");
+            }
+            
+            _name = value;
+        }
+    }
+
+    private int _price;
+    public int Price
+    {
+        get => _price;
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Цена не может быть отрицательной");
+            }
+
+            if (value == 0)
+            {
+                throw new ArgumentException("Цена не может быть 0");
+            }
+
+            _price = value;
+        }
+    }
+}
