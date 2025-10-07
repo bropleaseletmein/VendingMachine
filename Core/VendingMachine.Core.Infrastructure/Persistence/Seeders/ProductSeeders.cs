8c659153018607f08568bec2524bc5ebee705c53
andrew.severiukhin <and00998843@gmail.com>
2025-10-07 23:46:31 +0300

    add seeder

--- Core/VendingMachine.Core.Infrastructure/Persistence/Seeders/ProductSeeders.cs
+++ Core/VendingMachine.Core.Infrastructure/Persistence/Seeders/ProductSeeders.cs
@@ -1,6 +1,14 @@
+using VendingMachine.Core.Domain.Entities;
+
 namespace VendingMachine.Core.Infrastructure.Persistence.Seeders;
 
 public static class ProductSeeders
 {
-    
+    public static List<Product> GetDefaultProducts() =>
+    [
+        new Product { Id = 1, Name = "Вода", Price = 50, Amount = 10 },
+        new Product { Id = 2, Name = "Сок", Price = 80, Amount = 5 },
+        new Product { Id = 3, Name = "Шоколад", Price = 120, Amount = 3 },
+        new Product { Id = 4, Name = "Чипсы", Price = 90, Amount = 7 }
+    ];
 }
